8f57d880628df9e6519fdb0099f8f9b6d1505c22
Aiana <tashtandieva01@gmail.com>
2020-12-18 14:08:21 +0600

    final

--- game.py
+++ game.py
@@ -50,6 +50,127 @@ bulletX_change = 0
 bulletY_change = 10
 bullet_state = "ready"
 
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+textX = 10
+textY = 10
+
+
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+
+def show_score(x, y):
+    score = font.render("Score: " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def virus(x, y):
+    screen.blit(planeImg[i], (x, y))
+
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 50, y + 10))
+
+
+def iscollision(planex, planey, bulletx, bullety):
+    distance1 = math.sqrt(math.pow(planex-bulletx, 2))
+    distance2 = + (math.pow(planey-bullety, 2))
+    distance = distance1 + distance2
+    if distance < 27:
+        return True
+    else:
+        return False
+
+
+running = True
+while running:
+
+    screen.fill((0, 0, 0))
+
+    screen.blit(background, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -4
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 4
+            if event.key == pygame.K_SPACE:
+                if bullet_state == "ready":
+                    bullet_Sound = mixer.Sound('tank shoot.wav')
+                    bullet_Sound.play()
+
+                    # get the current x coordinate of protective-wear
+                    bulletX = playerX
+                    fire_bullet(bulletX, bulletY)
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    for i in range(num_of_plane):
+
+        if planeY[i] > 440:
+            for j in range(num_of_plane):
+                planeY[j] = 2000
+            game_over_text()
+            break
+
+        planeX[i] += planeX_change[i]
+        if planeX[i] <= 0:
+            planeX_change[i] = 1
+            planeY[i] += planeY_change[i]
+        elif planeX[i] >= 736:
+            planeX_change[i] = - 1
+            planeY[i] += planeY_change[i]
+
+        collision = iscollision(planeX[i], planeY[i], bulletX, bulletY)
+        if collision:
+            explosion_Sound = mixer.Sound('plane explosion.mp3')
+            explosion_Sound.play()
+            bulletY = 480
+            bullet_state = "ready"
+            score_value += 1
+            planeX[i] = random.randint(0, 736)
+            planeY[i] = random.randint(50, 150)
+
+        virus(planeX[i], planeY[i], )
+
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+
+    if bullet_state == "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
+
+    player(playerX, playerY)
+    show_score(textX, textY)
+
+    pygame.display.update()
+
+
 
 
 pygame.display.update()
